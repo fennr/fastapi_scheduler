81c8f6ecf60e802ec156bfbf0cc88357356bf732
v.merkurev <v.merkurev@promo-bot.ru>
2023-04-17 13:28:59 +0500

    fix: add Exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,36 @@
+from fastapi import HTTPException
+
+
+class SchedulerException(HTTPException):
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class UserException(SchedulerException):
+    """
+    Класс пользовательских ошибок
+    """
+
+
+class TaskException(SchedulerException):
+    """
+    Класс ошибок задач
+    """
+
+
+class RemindException(SchedulerException):
+    """
+    Класс ошибок напоминаний
+    """
+
+
+class UserNotFound(UserException):
+    ...
+
+
+class TaskNotFound(TaskException):
+    ...
+
+
+class RemindNotFound(RemindException):
+    ...
